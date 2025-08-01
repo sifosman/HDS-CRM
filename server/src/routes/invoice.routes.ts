82f5a0ec6d3a79e223ad4b88cca7faa0477ef3f5
Mohamed Osman <MOsman2@sars.gov.za>
2025-08-01 12:00:56 +0200

    friday8

--- server/src/routes/invoice.routes.ts
+++ server/src/routes/invoice.routes.ts
@@ -4,9 +4,9 @@ import invoiceController from '../controllers/invoice.controller';
 const router = express.Router();
 
 // Download invoice PDF for a quote
-router.get('/download/:quoteId', invoiceController.downloadInvoice.bind(invoiceController));
+router.get('/download/:quoteId', (req, res) => invoiceController.downloadInvoice(req, res));
 
 // Create invoice from payment (used by PayFast ITN handler)
-router.post('/create-from-payment', invoiceController.createInvoiceFromPayment.bind(invoiceController));
+router.post('/create-from-payment', (req, res) => invoiceController.createInvoiceFromPayment(req, res));
 
 export default router;
